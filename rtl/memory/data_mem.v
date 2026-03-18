4171635a19362932156b6389c5aa05f14cdbc87c
Lacus <2854651899@qq.com>
2026-03-18 15:14:28 +0800

    feat: 实现了DM和IM

--- rtl/memory/data_mem.v
+++ rtl/memory/data_mem.v
@@ -4,7 +4,22 @@ module DM (
     input mem_write,
     input [31:0] addr,
     input [31:0] w_data,
-    output [31:0] r_data
+    output reg [31:0] r_data
 );
-    
+    reg [31:0] mem [0:2047];
+
+    // 读操作
+    always @(*) begin
+        if (mem_read)
+            r_data = mem[addr[31:2]];
+        else
+            r_data = 32'b0;
+    end
+
+    // 写操作
+    always @(posedge clk) begin
+        if (mem_write)
+            mem[addr[31:2]] <= w_data;
+    end
+
 endmodule
--- rtl/memory/instr_mem.v
+++ rtl/memory/instr_mem.v
@@ -1,6 +1,8 @@
 module IM (
-    input [31:0] instr_add,
+    input [31:0] instr_addr,
     output [31:0] instr
 );
-    
+    reg [31:0] mem [0:2047];
+
+    assign instr = mem[instr_addr];
 endmodule
