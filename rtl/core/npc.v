c50ea3e41d40279d07ae6f194241371acb7363ed
Lacus <2854651899@qq.com>
2026-03-19 18:51:18 +0800

    refactor: 修改NPC的行为
    - 由端口npc_op决定NPC的跳转类型

--- rtl/core/npc.v
+++ rtl/core/npc.v
@@ -1,6 +1,6 @@
 module NPC (
     input [31:0] curr_pc,
-    input jmp,
+    input [1:0] npc_op,
     input cond_jmp,
     input [25:0] jmp_target,
     input [15:0] jmp_offset,
@@ -8,10 +8,15 @@ module NPC (
 );
     wire [31:0] pc_target = {curr_pc[31:28], jmp_target, 2'b00};
     wire [31:0] pc_offset = {14{jmp_offset[15]}, jmp_offset, 2'b00};
+    
+    localparam PC_PLUS4 = 2'b00;
+    localparam BRANCH_JMP = 2'b01;
+    localparam DIRECT_JMP = 2'b10;
+
     always @(*) begin
-        if (jmp)
+        if (npc_op == DIRECT_JMP)
             next_pc = pc_target;
-        else if (cond_jmp)
+        else if ((npc_op == BRANCH_JMP) && cond_jmp)
             next_pc = curr_pc + pc_offset;
         else
             next_pc = curr_pc + 4;
