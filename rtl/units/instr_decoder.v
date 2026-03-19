bda3f0838e2ef9a8f54a60c9e42be92f41fa4ca0
Lacus <2854651899@qq.com>
2026-03-19 19:18:18 +0800

    feat: 添加指令解析器

---
+++ rtl/units/instr_decoder.v
@@ -0,0 +1,22 @@
+module InstrDecoder (
+    input [31:0] instr,
+    output [5:0] op,
+    output [5:0] funct,
+    output [4:0] shamt,
+    output [4:0] rs, rt, rd,
+    output [15:0] imm16,
+    output [25:0] instr_index,
+);
+    assign op = instr[31:26];
+
+    assign instr_index = instr[25:0];
+
+    assign rs = instr[25:21];
+    assign rt = instr[20:16];
+    assign rd = instr[15:11];
+
+    assign imm16 = instr[15:0];
+    
+    assign shamt = instr[10:6];
+    assign funct = instr[5:0];
+endmodule
